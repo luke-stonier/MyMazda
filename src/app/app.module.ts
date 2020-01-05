5b81328ee5702288659925190ba82fed4c443bb0
luke stonier <lukestonier@hotmail.com>
2020-01-05 21:55:47 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ColourComponent } from "./item/colour.component";
+import { HttpClientModule } from "@angular/common/http";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptFormsModule,
+        NativeScriptModule,
+        AppRoutingModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        ColourComponent
+    ],
+    providers: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
---
+++ src/app/item/colour.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from "@angular/core";
+import { Slider } from "tns-core-modules/ui/slider";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Color } from "tns-core-modules/color/color";
+
+export class MMColour {
+    public r: number = 10;
+    public g: number = 10;
+    public b: number = 10;
+    public d: number = 1;
+
+    constructor() {}
+
+    public createQueryString(): string {
+        return `?r=${this.r.toFixed(0)}&g=${this.g.toFixed(0)}&b=${this.b.toFixed(0)}&d=${this.d.toFixed(0)}`;
+    }
+
+    public toHex(): string {
+        try {
+        const colour: Color = new Color(0, this.r, this.g, this.b);
+        return `#${colour.hex.substring(3)}`;
+        } catch(ex) {
+            return '#FFFFFF';
+        }
+    }
+}
+
+@Component({
+    selector: "mm-colour",
+    templateUrl: "./colour.component.html"
+})
+export class ColourComponent implements OnInit {
+
+    public ourColour: MMColour = new MMColour();
+    public test: string = 'red';
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+
+    }
+
+    public updateColour() {
+        this.makeUpdateColourRequest().subscribe((r) => {
+            console.log(r);
+        });
+    }
+
+    private makeUpdateColourRequest(): Observable<any> {
+        const requestUrl = `192.168.1.4/${this.ourColour.createQueryString()}`;
+        console.log(`Making request to: ${requestUrl}`);
+        return this.http.get(requestUrl);
+    }
+
+    onRSliderValueChange(args) {
+        let slider = <Slider>args.object;
+        this.ourColour.r = slider.value;
+    }
+
+    onGSliderValueChange(args) {
+        let slider = <Slider>args.object;
+        this.ourColour.g = slider.value;
+    }
+
+    onBSliderValueChange(args) {
+        let slider = <Slider>args.object;
+        this.ourColour.b = slider.value;
+    }
+}
